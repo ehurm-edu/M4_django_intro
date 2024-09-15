0261132ee77011e9a743e9d68d277f1e80facd4c
Erin Hurm <ehurm@ivytech.edu>
2024-09-15 14:36:09 -0500

    additional files

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
+# code from https://tutorial.djangogirls.org/en/django_admin/
 
-# Register your models here.
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post)
